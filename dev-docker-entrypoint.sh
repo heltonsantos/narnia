a26235e81107afa4de5d5337e8df478f5fc43935
Helton Santos <heltondas@gmail.com>
2023-05-14 20:52:28 -0300

    initial template

---
+++ dev-docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle check || bundle install
+
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
+bundle exec rake db:seed
+
+exec "$@"
