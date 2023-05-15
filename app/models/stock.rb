45a0c7ec0c29cf2fa3ab416ed2cfe9465aced88d
Helton Santos <heltondas@gmail.com>
2023-05-14 21:59:02 -0300

    add stock status

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,17 @@ class Stock < ApplicationRecord
   has_one :client, through: :wallet
 
   validates :uuid, :kind, :status, presence: true
+
+  aasm column: :status do
+    state :available, initial: true
+    state :locked
+
+    event :lock do
+      transitions from: :available, to: :locked
+    end
+
+    event :available do
+      transitions from: :lock, to: :available
+    end
+  end
 end
