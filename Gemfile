5db3286ebbc354628073a6b97559030067487470
Helton Santos <heltondas@gmail.com>
2023-05-18 12:43:35 -0300

    add paper trail

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'sidekiq'
 
 gem 'aasm', '5.2.0'
 
+gem 'paper_trail'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_203215) do
+ActiveRecord::Schema.define(version: 2023_05_18_154259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(version: 2023_05_15_203215) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["uuid"], name: "index_transactions_on_uuid", unique: true
     t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   create_table "wallets", force: :cascade do |t|
