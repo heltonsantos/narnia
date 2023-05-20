ba1f19a5293f900112fd925812e770066d48146b
Helton Santos <heltondas@gmail.com>
2023-05-20 10:47:05 -0300

    fix entrypoint

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 bundle check || bundle install
 
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:migrate:reset
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
 bundle exec rake db:seed
 
 exec "$@"
